fae11c91cffe1f42efe8f1b7d1815f0c63f299a6
curdicu <curdicu@gmail.com>
2025-02-27 23:52:50 +0800

    1

--- Package.swift
+++ Package.swift
@@ -15,8 +15,10 @@ let package = Package(
         dependencies: [
             .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0")),
             .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.6.2")),
-            .package(url: "https://github.com/curdicu/UIExtensions.git", .branch( "main")),
-            .package(url: "https://github.com/curdicu/HsExtensions.git", .branch( "main")),
+            // .package(url: "https://github.com/curdicu/UIExtensions.git", .branch( "main")),
+            // .package(url: "https://github.com/curdicu/HsExtensions.git", .branch( "main")),
+            .package(url: "https://github.com/horizontalsystems/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.1")),
+            .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
         ],
         targets: [
             .target(
@@ -24,8 +26,8 @@ let package = Package(
                     dependencies: [
                         "Alamofire",
                         "Kingfisher",
-                        .product(name: "UIExtensions", package: "UIExtensions"),
-                        .product(name: "HsExtensions", package: "HsExtensions"),
+                        .product(name: "UIExtensions", package: "UIExtensions.Swift"),
+                        .product(name: "HsExtensions", package: "HsExtensions.Swift"),
                     ]
             ),
         ]
