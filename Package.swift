07c6781985f7e9fe017c97db1dc690dc1d4d9e02
curdicu <curdicu@gmail.com>
2025-03-01 16:15:27 +0800

    *

--- Package.swift
+++ Package.swift
@@ -15,8 +15,6 @@ let package = Package(
         dependencies: [
             .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.4.0")),
             .package(url: "https://github.com/onevcat/Kingfisher.git", .upToNextMajor(from: "7.6.2")),
-            // .package(url: "https://github.com/curdicu/UIExtensions.git", .branch( "main")),
-            // .package(url: "https://github.com/curdicu/HsExtensions.git", .branch( "main")),
             .package(url: "https://github.com/horizontalsystems/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.1")),
             .package(url: "https://github.com/horizontalsystems/HsExtensions.Swift.git", .upToNextMajor(from: "1.0.6")),
         ],
